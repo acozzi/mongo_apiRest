ad19df6a8fa37a2971428d7f7207ee858087b9ac
acozzi <>
2022-11-16 20:58:05 +0000

    add breakpoints

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const uri = process.env.USER + process.env.PASSWORD + process.env.CLUSTER;
 MongoClient.connect(uri, function(err, db) {
   if (err) throw err;
   var dbo = db.db("climate");
-  var query = {province: /San/};
+  var query = {name: "Venado Tuerto"};
   dbo.collection("smn").find(query).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
@@ -19,27 +19,3 @@ MongoClient.connect(uri, function(err, db) {
     db.close();
   });
 });
-
-
-
-
-/*
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://acozzi:process.env.PASSWORD@clusterale.3clkqzg.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(
-  uri, 
-  { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    serverApi: ServerApiVersion.v1 
-  });
-client.connect(
-  err => {
-    const collection =          
-      client.db("climate").collection("smn");
-      console.log("Conexion realizada");
-      console.log(client.db.smn.find({name: "Dolores"}))
-    
-  // perform actions on the collection object
-  client.close();
-});*/
